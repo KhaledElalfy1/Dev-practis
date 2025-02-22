0754a2e3775faf26db95e41a8de9504143a90fb6
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-22 20:24:10 +0200

    add api error handler

--- lib/core/network/api_result.dart
+++ lib/core/network/api_result.dart
@@ -1,3 +1,4 @@
+import 'package:dev_practice/core/network/api_error_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'api_result.freezed.dart';
@@ -5,5 +6,5 @@ part 'api_result.freezed.dart';
 @freezed
 class ApiResult<T> with _$ApiResult<T> {
   factory ApiResult.success(T data) = Success<T>;
-  factory ApiResult.failure(String message) = Failure;
+  factory ApiResult.failure(ApiErrorModel apiErrorModel) = Failure;
 }
